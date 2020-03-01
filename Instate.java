820478325a50896f84725831299f7df95c25d401
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2020-02-29 23:45:40 -0500

    Update Instate.java

--- Instate.java
+++ Instate.java
@@ -1,20 +1,42 @@
 /**
+ * 
+ * This class overrides the toString() method in order to store information of
+ * an instate student and overrides tuitionDue() method in order to compute how
+ * much an instate student will have to pay.
+ * 
  * @author Shivam Patel
  * @author Kevin Shah
  */
 public class Instate extends Student {
     private int funds;
 
+    /**
+     * Constructor initializes the first name, last name, credits, and the amound of
+     * funds for a student.
+     * 
+     * @param fname
+     * @param lname
+     * @param credit
+     * @param funds
+     */
     public Instate(String fname, String lname, int credit, int funds) {
         super(fname, lname, credit);
         this.funds = funds;
     }
 
+    /**
+     * This method overrides the Student toString method in order to include the
+     * tuition due for the instate student.
+     */
     @Override
     public String toString() {
         return super.toString() + " tuition due: " + "$" + this.tuitionDue();
     }
 
+    /**
+     * This method overrides the abstract method of the student class in order to
+     * compute the amount of tuition for an instate student.
+     */
     @Override
     public int tuitionDue() {
         int tuition = 0;
@@ -33,12 +55,25 @@ public class Instate extends Student {
         return tuition;
     }
 
+    /**
+     * This main testbed method tests the constructor and the tuitionDue() and the
+     * toString method.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
 
         Instate student = new Instate("John", "Smith", 17, 1000);
+        Instate student1 = new Instate("Shivam", "Patel", 15, 0);
+        Instate student2 = new Instate("Kevin", "Shah", 9, 2000);
 
         System.out.println(student.toString());
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
+
         System.out.println(student.tuitionDue());
+        System.out.println(student1.tuitionDue());
+        System.out.println(student2.tuitionDue());
 
     }
 
